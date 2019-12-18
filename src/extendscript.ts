3111bc02967891e8e319707c5b17be5f6f5f6b52
KaivnD <KaivnD@hotmail.com>
2019-12-18 16:41:46 +0800

    feat:压缩转换过后的js

--- src/extendscript.ts
+++ src/extendscript.ts
@@ -1,6 +1,10 @@
 import prependify from "prependify";
 import browserify from "browserify";
+import UglifyJS from "uglify-js"
+import rds from 'randomstring'
+import path from 'path'
 import fs from 'fs'
+import os from 'os'
 
 export class Extendscript {
   public plugins: any[];
@@ -25,27 +29,54 @@ export class Extendscript {
   /**
    * Parser
    */
-  public Parse(fileIn: string, fileOut: string) {
-    const b = browserify({
-      entries: [fileIn],
-      transform: [
-        [
-          require.resolve("babelify"),
-          {
-            presets: ["@babel/preset-env"],
-            plugins: [
-              require.resolve(
-                "babel-plugin-transform-es3-member-expression-literals"
-              ),
-              require.resolve("babel-plugin-transform-es3-property-literals"),
-              require.resolve("babel-plugin-transform-es5-property-mutators")
-            ]
-          }
-        ]
-      ],
-      plugin: this.plugins
-    });
+  public Parse(fileIn: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      if (!fs.existsSync(fileIn)) {
+        reject(new Error('Input file is not found'))
+        return
+      }
+      const b = browserify({
+        entries: [fileIn],
+        transform: [
+          [
+            require.resolve("babelify"),
+            {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                require.resolve(
+                  "babel-plugin-transform-es3-member-expression-literals"
+                ),
+                require.resolve("babel-plugin-transform-es3-property-literals"),
+                require.resolve("babel-plugin-transform-es5-property-mutators")
+              ]
+            }
+          ]
+        ],
+        plugin: this.plugins
+      });
 
-    b.bundle().pipe(fs.createWriteStream(fileOut));
+      const tmpfile = path.join(os.tmpdir(), rds.generate({
+        length: 12,
+        charset: 'alphanumeric'
+      }) + '.jsx')
+
+      const fws = fs.createWriteStream(tmpfile)
+
+      fws.on('close', () => {
+        const res = UglifyJS.minify(fs.readFileSync(tmpfile, {encoding: 'utf-8'}), {
+          mangle: {
+            eval: true
+          }
+        })
+        if (res.error) reject(res.error)
+        else {
+          fs.unlinkSync(tmpfile)
+          fs.writeFileSync(tmpfile, res.code, {encoding: 'utf-8'})
+          resolve(tmpfile)
+        }
+      })
+
+      b.bundle().pipe(fws);
+    })
   }
 }
